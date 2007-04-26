d0361b47c425e741f2898c5cefbae3a7aa879c1b
Jim Garlick <garlick.jim@gmail.com>
2007-04-26 00:10:30 +0000

    remove isp.old

--- isp.old/test/test1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-test -x /usr/bin/xmllint || exit 2
-
-touch testfile1 testfile2 testfile3
-
-ispcat testfile1 testfile2 testfile3 | tee xml.out | xmllint -noout - || exit 1
-exit 0
--- isp.old/test/test9.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-srcxml 2 100 100 | sinkxml 2 100 100 2 || exit 1
-
-exit 0
