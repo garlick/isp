c5c31f87856aaa1003254720c69fef3408a005e8
garlick <garlick@708a654e-7f2e-0410-8ef1-b6e626d2094b>
2007-04-26 00:10:30 +0000

    remove isp.old

--- trunk/isp.old/test/test10.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-test -x /usr/bin/srun || exit 2
-
-srcxml 2 $1 $2 | /usr/bin/srun -u -N1 dd bs=$3 | sinkxml 2 $1 $2 || exit 1
-
-exit 0
--- trunk/isp.old/test/test7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-srcxml 4 $1 $2 | dd bs=$3 | sinkxml 4 $1 $2 || exit 1
-
-exit 0
